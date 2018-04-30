478d9a7fd47a6adb73ab345371fc5ad2f71551df
KarapetyanSergo <sergo.karapetyan.00@mail.ru>
2018-04-30 03:32:59 -0700

    js code

--- Compass/www/js/index.js
+++ Compass/www/js/index.js
@@ -1,18 +1,28 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
+var obj = null;
+
 function onDeviceReady() {
-    document.getElementById("button").onclick = function () {
-        navigator.compass.getCurrentHeading(onSuccess, onError);
-    }
+   navigator.compass.getCurrentHeading(onSuccess, onError)
 }
 
 function onSuccess(heading) {
-    var head = heading.magneticHeading;
-    console.log(head);
-    document.getElementById("arrow").style.transform = "rotate("+head+"deg)"
-
+    var head =  heading.magneticHeading;
+    document.getElementById("arrow").style.transform = "rotate("+head+"deg)";
+    console.log(head)
 }
 
 function onError(compassError) {
-    alert('Compass Error: ' + compassError.code);
+    alert('Compass error: ' + compassError.code);
+}
+
+function func() {
+    if(obj == null){
+        navigator.compass.clearWatch(obj);
+        obj+null;
+        console.log("ha")
+    }
+    else{
+        obj = navigator.compass.watchHeading(onSuccess, onError, {frequency: 1000})
+    }
 }
